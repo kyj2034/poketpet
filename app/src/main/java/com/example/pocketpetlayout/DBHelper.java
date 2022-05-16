181730fe16e6fe582200467cfc5e5db89d23423f
hyunsus <dkrl0257@naver.com>
2022-05-16 22:56:47 +0900

    Profile 추가

--- app/src/main/java/com/example/pocketpetlayout/DBHelper.java
+++ app/src/main/java/com/example/pocketpetlayout/DBHelper.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 
 public class DBHelper extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 3;
     public static final String DATEBASE_NAME = "PoketPet.db";
 
     public DBHelper(Context context) {
